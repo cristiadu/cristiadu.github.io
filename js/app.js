6ddbe478dcbdcd98d83806860ed108a918a100c2
Cristiano Faustino <cristiadu@gmail.com>
2015-07-15 13:31:58 -0300

    Refactoring the application to use AngularJS, and changing layout

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var portfolioApp = angular.module('portfolioApp', []);
+
+portfolioApp.controller('skillsController',['$scope','$http',function($scope,$http){
+	$http.get('json/skills.json').success(function(data){
+		$scope.skills = data;
+	});
+}]);
+
+portfolioApp.controller('projectsController',['$scope','$http',function($scope,$http){
+	$http.get('json/projects.json').success(function(data){
+		$scope.projects = data;
+	});
+}]);
