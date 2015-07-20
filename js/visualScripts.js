7d7abcdb3915013f068fbdce1de9f56027cb2750
Cristiano Faustino <cristiadu@gmail.com>
2015-07-20 12:36:46 -0300

    Reducing offset again

--- js/visualScripts.js
+++ js/visualScripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 			    var $target = $(target);
 
 			    $('html, body').stop().animate({
-			        'scrollTop': $target.offset().top - 200
+			        'scrollTop': $target.offset().top - 100
 			    }, 900, 'swing', function () {
 			        window.location.hash = target;
 			    });
