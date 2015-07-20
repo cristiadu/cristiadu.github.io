708fda44930682d6ae3f69665e533f2c28ae0903
Cristiano Faustino <cristiadu@gmail.com>
2015-07-20 12:43:38 -0300

    Reverting offset

--- js/visualScripts.js
+++ js/visualScripts.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
 			    var $target = $(target);
 
 			    $('html, body').stop().animate({
-			        'scrollTop': $target.offset().top - 90
+			        'scrollTop': $target.offset().top - 100
+
 			    }, 900, 'swing', function () {
 			        window.location.hash = target;
 			    });
