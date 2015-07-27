cd45e854b479a823fc82f7d007253f79ab100484
Cristiano Faustino <cristiadu@gmail.com>
2015-07-27 15:04:34 -0300

    Showing always opened the first project on the list

--- js/visualScripts.js
+++ js/visualScripts.js
@@ -13,7 +13,13 @@ $(document).ready(function(){
 			    });
 			});
 
+		/* This code closes the collapsed menu when the user clicks on a link*/
 		$('.navbar-collapse').click('li', function() {
 		    $('.navbar-collapse').collapse('hide');
+		});
+
+		// This code shows opened the first project on the project's list
+		$("panel-collapse").first(function(){
+			$(this).collapse('show');
 		});
 });
