a19a8f7564ddafb6cbc3930807ffe18c482283e1
Cristiano Faustino <cristiadu@gmail.com>
2019-12-01 22:44:51 -0800

    changing color scheme of whole website

--- js/visualScripts.js
+++ js/visualScripts.js
@@ -1,21 +1,22 @@
-$(document).ready(function(){
-		$('a[href*="#"]').on('click',function (e) {
-			    e.preventDefault();
+$(document).ready(function () {
+	$('a[href*="#"]').on('click', function(e) {
+		e.preventDefault();
 
-			    var target = this.hash;
-			    var $target = $(target);
+		const target = this.hash;
+		const $target = $(target);
 
-			    $('html, body').stop().animate({
-			        'scrollTop': $target.offset().top - 100
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top - 100
 
-			    }, 900, 'swing', function () {
-			        window.location.hash = target;
-			    });
-			});
-
-		/* This code closes the collapsed menu when the user clicks on a link*/
-		$('.navbar-collapse').click('li', function() {
-		    $('.navbar-collapse').collapse('hide');
+		}, 900, 'swing', function () {
+			window.location.hash = target;
 		});
+	});
+
+
+	/* This code closes the collapsed menu when the user clicks on a link*/
+	$('.navbar-collapse').click('li', function () {
+		$('.navbar-collapse').collapse('hide');
+	});
 
 });
