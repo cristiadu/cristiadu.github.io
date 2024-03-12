89c7f90dc8ef7d1a136af34ccfdd820262361d1f
Cristiano Faustino <cristiano@giftbit.com>
2024-03-12 12:23:21 -0700

    some minor layout fixes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <div className="container-fluid">
-                <div className="col-md-6 col-xs-12">Cristiano Faustino's Portfolio © 2024. All rights reserved.</div>
-                <div className="col-md-6 col-xs-12 pull-right">Email: <a href="mailto:cristiadu@gmail.com">cristiadu@gmail.com</a>
+            <div class="col-md-4"></div>
+            <div class="container-fluid col-md-8">
+                <div class="col-md-7 col-xs-12">Cristiano Faustino's Portfolio © 2024. All rights reserved.</div>
+                <div class="col-md-4 col-xs-12">Email: <a href="mailto:cristiadu@gmail.com">cristiadu@gmail.com</a>
                 </div>
             </div>
         </footer>
