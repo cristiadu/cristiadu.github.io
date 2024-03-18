a23a2ca6de067acdc2afc0b152f313f585e75644
Cristiano Faustino <cristiadu@gmail.com>
2024-03-17 19:59:50 -0700

    removing jquery styling in favor of some better options (#37)

--- src/components/layout/HeaderNavBar.jsx
+++ src/components/layout/HeaderNavBar.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
 
 const HeaderNavBar = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const handleToggle = () => {
+        setIsCollapsed(!isCollapsed);
+    }
+
+    const closeMenu = () => {
+        setIsCollapsed(true);
+    }
+
     return (
         <header>
             <nav className="navbar navbar-default navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu"
-                            aria-expanded="false">
+                        <button type="button" className={`navbar-toggle ${isCollapsed ? 'collapsed' : ''}`} onClick={handleToggle}
+                            aria-expanded={!isCollapsed}>
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
@@ -20,20 +31,20 @@ const HeaderNavBar = () => {
                         </div>
                     </div>
 
-                    <div id="menu" className="navbar-collapse collapse">
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a href="https://drive.google.com/file/d/1kERzdkx42MpyvROyGb-dNlfglavPgzDc/view?usp=sharing"
+                    <div id="menu" className={`navbar-collapse ${isCollapsed ? 'collapse' : ''}`}>
+                        <ul className="nav navbar-nav navbar-right" onClick={closeMenu}>
+                            <li><a href="https://drive.google.com/file/d/1cD4wZK0Z4qqOXOH_8XwoIVWl1uVh9lZ-/view?usp=sharing"
                                 target="_blank"><i className="fa fa-user"></i> Resume</a></li>
-                            <li><a href="#projects"><i className="fa fa-cogs"></i> Projects</a></li>
-                            <li><a href="#skills"><i className="fas fa-chess-rook"></i> Skills</a></li>
+                            <li><Link as='a' to="projects" spy={true} smooth={true} offset={-100} duration={900}><i className="fa fa-cogs"></i> Projects</Link></li>
+                            <li><Link as='a' to="skills" spy={true} smooth={true} offset={-100} duration={900}><i className="fa fa-chess-rook"></i> Skills</Link></li>
                             <li><a href="https://www.linkedin.com/in/cristianofaustino/" target="_blank"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
-                            <li><a href="#contact"><i className="fa fa-commenting-o"></i> Contact</a></li>
+                            <li><Link as='a' to="contact" spy={true} smooth={true} offset={-100} duration={900}><i className="fa fa-commenting-o"></i> Contact</Link></li>
                         </ul>
                     </div>
                 </div>
             </nav>
         </header>
-        )
+    )
 }
 
 export default HeaderNavBar;
