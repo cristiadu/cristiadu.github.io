476744d53acc8278366ac49484d794604cfa5ba7
Cristiano Faustino <cristiadu@gmail.com>
2024-04-02 22:49:31 -0700

    oops i did it again

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,7 +4,7 @@ import { Col, Card } from 'react-bootstrap';
 
 const Skill = ({ skill }) => {
     return ( 
-        <Col md={4} sm={3} xs={6}>
+        <Col md={3} sm={4} xs={6}>
             <Card  className="skill-item">
                 <Card.Img variant="top" src={'images/' + skill.image} alt={skill.text} />
             </Card>
