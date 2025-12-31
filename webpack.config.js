fe134b061644f3453fa904b2538a72c08ece536e
Cristiano Faustino <cristiano@giftbit.com>
2025-12-31 13:39:33 -0800

    tests

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
