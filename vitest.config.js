a014077dfc19418e72de6a21c0b6bc034205b4cd
Cristiano Faustino <cristiano@giftbit.com>
2026-03-18 01:04:48 -0700

    chore: remove stale vitest esbuild jsx config

--- vitest.config.js
+++ vitest.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vitest/config'
 import path from 'path'
 
 export default defineConfig({
-  esbuild: {
-    jsx: 'automatic',
-    jsxImportSource: 'react'
-  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/tests/setup.js'],
